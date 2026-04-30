070edb7c10be64c3f43f7ab353b360426b1aafa0
czxieddan <czxieddan@gmail.com>
2026-04-30 20:56:01 +0800

    Chore: add AGPL-3.0 license headers and bump version to 1.0.1

--- src/domain.rs
+++ src/domain.rs
@@ -1,3 +1,24 @@
+//------------------------------------------------------------------------------------
+// domain.rs -- Part of RGMR
+//
+// Copyright (C) 2026 CzXieDdan. All rights reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// https://github.com/czxieddan/RGMR
+//------------------------------------------------------------------------------------
+
 pub use analysis::{AnalysisOutcome, AnalysisRawResponse, AnalysisRequest};
 pub use config::{
     ApiConfig, AppConfig, ConfigValidation, DEFAULT_BASE_URL, DEFAULT_TIMEOUT_SECS, PromptProfile,
@@ -31,7 +52,7 @@ pub mod config {
     pub const DEFAULT_TIMEOUT_SECS: u64 = 45;
 
     fn default_version() -> String {
-        "1.0.0".to_owned()
+        "1.0.1".to_owned()
     }
 
     fn default_base_url() -> String {
